ddcab01abf73e8b9ee404f75a27ff032e8674a52
allen <eepson123@gmail.com>
2021-02-23 21:53:14 +0800

    修正助教通知

--- app/js/weekone.js
+++ app/js/weekone.js
@@ -38,29 +38,29 @@ const sun = 1;
 let cloud = 3;
 
 if (!trafficLightStatus) {
-    console.log(`現在是紅燈,馬路上有${people}個人,天上有${sun}個,${cloud}朵雲!`)
+    console.log(`現在是紅燈,馬路上有${people}個人,天上有${sun}個太陽,${cloud}朵雲!`)
 };
 
 //第五題 看圖宣告變數
 
 let point = 50;
-const title = "【最終試煉】 使用jQuery 打造互動性網頁動畫效果";
+const taskTitle = "【最終試煉】 使用jQuery 打造互動性網頁動畫效果";
 let passPeopleNum = 62;
 let passCurrentDay = 16;
 let passCurrentTime = 9;
 const cardImg = "./img/*.png";
 
-console.log(point, title, passPeopleNum, passCurrentDay, passCurrentTime, cardImg);
+console.log(point, taskTitle, passPeopleNum, passCurrentDay, passCurrentTime, cardImg);
 
 
 //第六題 變數重新賦值
 
 let a = 1; //宣告 a 變數 賦值1
 let b = 0; //宣告 b 變數 賦值0
-a = 3; //a 變數賦值 3 
-a = b + 2; // a變數 賦值 0 + 2 目前 a為 2 
+a = 3; //a 變數重新賦值為 3 
+a = b + 2; // a變數 賦值 0 + 2 目前 a重新賦值為2 
 a - b; // 2 - 0 
-b += 1; // b賦值 為 1  b= 0+1; 目前 b為 1
+b += 1; // b賦值 為 1  b= 0+1; 目前 b重新賦值為 1
 console.log(a, b);
 
 
