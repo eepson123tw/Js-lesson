30f52e26254cc7b2171de9da46e470c312cccd74
allen <eepson123@gmail.com>
2021-02-25 01:20:28 +0800

    新增猜拳機器人 跟第二周主線

--- app/js/all.js
+++ app/js/all.js
@@ -25,16 +25,19 @@
         target.classList.toggle('title__text--active')
 
 
-
     }
     //在本頁時加上標籤顏色
     const checkPage = function() {
 
         let pageData = document.querySelectorAll('[data-page]');
 
+
         pageData.forEach((page) => {
 
+            let target = page.dataset.page;
+
             if (page.dataset.page == pageName) {
+                console.log(page, page.dataset.page)
 
                 page.classList.add('title__text--active');
 
@@ -44,9 +47,6 @@
 
 
     }
-
     checkPage();
-
-
 
 })()
