31c8703ad50bf14e935388d305c011ba2cc11938
allen <eepson123@gmail.com>
2021-03-26 00:59:08 +0800

    done

--- app/js/all.js
+++ app/js/all.js
@@ -1,52 +1,50 @@
 ;
 (function() {
 
-  const pageName = document.title;
+    const pageName = document.title;
 
-  console.log(`目前頁面為:${pageName}`);
-
-
-
-  let navbar = document.querySelectorAll(".title__text");
+    console.log(`目前頁面為:${pageName}`);
 
 
 
-  navbar.forEach((item) => {
-
-    item.addEventListener('click', barClick);
+    let navbar = document.querySelectorAll(".title__text");
 
 
-  })
 
-  function barClick(e) {
+    navbar.forEach((item) => {
 
-    let target = e.target;
+        item.addEventListener('click', barClick);
 
-    target.classList.toggle('title__text--active')
-
-
-  }
-  //在本頁時加上標籤顏色
-  const checkPage = function() {
-
-    let pageData = document.querySelectorAll('[data-page]');
-
-
-    pageData.forEach((page) => {
-
-      let target = page.dataset.page;
-
-      if (page.dataset.page == pageName) {
-
-
-        page.classList.add('title__text--active');
-
-      }
 
     })
 
+    function barClick(e) {
 
-  }
-  checkPage();
+        let target = e.target;
+
+        target.classList.toggle('title__text--active')
+
+
+    }
+    //在本頁時加上標籤顏色
+    const checkPage = function() {
+
+        let pageData = document.querySelectorAll('[data-page]');
+
+        pageData.forEach((page) => {
+
+            let target = page.dataset.page;
+
+            if (page.dataset.page == pageName) {
+
+                page.classList.add('title__text--active');
+
+            }
+
+        })
+
+
+    }
+    checkPage();
 
 })()
